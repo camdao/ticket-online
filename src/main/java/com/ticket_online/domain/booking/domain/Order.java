31420e438c3fb236daa9f6740c9e96d8d294f22e
camdao <luongcamdao@gmail.com>
2026-02-17 10:22:13 +0700

    feat: domain Order

---
+++ src/main/java/com/ticket_online/domain/booking/domain/Order.java
@@ -0,0 +1,37 @@
+package com.ticket_online.domain.booking.domain;
+
+import com.ticket_online.domain.model.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Long showId;
+
+    private OrderStatus status;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    Order(Long userId, Long showId, OrderStatus status) {
+        this.userId = userId;
+        this.showId = showId;
+        this.status = status;
+    }
+
+    public static Order createOrder(Long userId, Long showId) {
+        return Order.builder().userId(userId).showId(showId).status(OrderStatus.PENDING).build();
+    }
+}
