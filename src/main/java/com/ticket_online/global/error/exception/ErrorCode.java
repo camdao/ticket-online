7416385deb5327d246b8872d24bf08306cc1f188
camdao <luongcamdao@gmail.com>
2026-04-06 19:33:19 +0700

    fix: ; in ErrorCode

--- src/main/java/com/ticket_online/global/error/exception/ErrorCode.java
+++ src/main/java/com/ticket_online/global/error/exception/ErrorCode.java
@@ -44,7 +44,7 @@ public enum ErrorCode {
     PASSWORD_NOT_MATCHES(HttpStatus.BAD_REQUEST, "The provided password does not match."),
 
     // Show
-    SHOW_NOT_FOUND(HttpStatus.NOT_FOUND, "The requested show was not found."),
+    SHOW_NOT_FOUND(HttpStatus.NOT_FOUND, "The requested show was not found.");
 
     private final HttpStatus status;
     private final String message;
