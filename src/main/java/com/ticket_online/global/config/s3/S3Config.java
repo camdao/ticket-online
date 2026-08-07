20f9a09cc302f64f05216b7b300fdb3c6c4ab783
camdao <luongcamdao@gmail.com>
2026-08-07 15:54:23 +0700

    feat: config s3

---
+++ src/main/java/com/ticket_online/global/config/s3/S3Config.java
@@ -0,0 +1,46 @@
+package com.ticket_online.global.config.s3;
+
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+    private final S3Properties s3Properties;
+
+    private AwsBasicCredentials getCredentials() {
+        return AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey());
+    }
+
+    private S3Configuration getConfiguration() {
+        return S3Configuration.builder().pathStyleAccessEnabled(true).build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(s3Properties.endpoint()))
+                .credentialsProvider(StaticCredentialsProvider.create(getCredentials()))
+                .region(Region.of(s3Properties.region()))
+                .serviceConfiguration(getConfiguration())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .endpointOverride(URI.create(s3Properties.endpoint()))
+                .credentialsProvider(StaticCredentialsProvider.create(getCredentials()))
+                .region(Region.of(s3Properties.region()))
+                .serviceConfiguration(getConfiguration())
+                .build();
+    }
+}
