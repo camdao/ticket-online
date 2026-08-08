17052f2d933788de36e2abc8e2e7a54190a6d800
camdao <75600481+camdao@users.noreply.github.com>
2026-08-08 19:56:38 +0700

    feat:s3 (#65)
    * fix: payment
    
    * fix: bookings, remove Hold seats api
    
    * feat: booking Only return CONFIRMED
    
    * feat: config s3
    
    * feat: add ImageUtil

--- src/main/java/com/ticket_online/global/config/properties/PropertiesConfig.java
+++ src/main/java/com/ticket_online/global/config/properties/PropertiesConfig.java
@@ -1,11 +1,17 @@
 package com.ticket_online.global.config.properties;
 
 import com.ticket_online.global.config.redis.RedisProperties;
+import com.ticket_online.global.config.s3.S3Properties;
 import com.ticket_online.global.config.vnpay.VnpayProperties;
 import com.ticket_online.global.jwt.JwtProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({RedisProperties.class, JwtProperties.class, VnpayProperties.class})
+@EnableConfigurationProperties({
+    RedisProperties.class,
+    JwtProperties.class,
+    VnpayProperties.class,
+    S3Properties.class
+})
 @Configuration
 public class PropertiesConfig {}
