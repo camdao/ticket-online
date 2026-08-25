451fa566a957ef2ec982154a031b9343e421a5a2
camdao <luongcamdao@gmail.com>
2026-08-25 18:13:36 +0700

    chore: re-run jobs ci/cd

--- src/main/java/com/ticket_online/global/error/exception/ErrorCode.java
+++ src/main/java/com/ticket_online/global/error/exception/ErrorCode.java
@@ -84,7 +84,7 @@ public enum ErrorCode {
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "The requested payment was not found."),
     PAYMENT_ALREADY_COMPLETED(HttpStatus.CONFLICT, "Payment is already completed."),
     INVALID_PAYMENT_CALLBACK(HttpStatus.BAD_REQUEST, "Invalid payment callback signature."),
-    IMAGE_FILE_EXTENSION_NOT_FOUND(HttpStatus.NOT_FOUND, "");
+    IMAGE_FILE_EXTENSION_NOT_FOUND(HttpStatus.NOT_FOUND, ".");
 
     private final HttpStatus status;
     private final String message;
